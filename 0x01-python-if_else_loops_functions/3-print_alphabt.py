11e6c879b04e759ad0efcc53d4693ba25b2c87bc
Zakaria Bakour <husa.bakour02@gmail.com>
2023-11-28 13:31:42 -0800

    wip

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,7 +4,7 @@ string = ""
 for i in range(97, 123):
     if i != 113 or i != 101:
         continue
-
-    string += chr(i)
+    else:
+        string += chr(i)
 
 print(string.format(string), end="")
