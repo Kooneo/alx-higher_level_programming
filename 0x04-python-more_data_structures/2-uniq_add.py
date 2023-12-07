c936b2069939e11eee088505be70f6018a9edfb4
Zakaria Bakour <husa.bakour02@gmail.com>
2023-12-06 16:37:50 -0800

    wip

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    uniq_list = set(my_list)
     total_nums = 0
 
-    for num in my_list:
+    for num in uniq_list:
         total_nums += num
 
     return total_nums
