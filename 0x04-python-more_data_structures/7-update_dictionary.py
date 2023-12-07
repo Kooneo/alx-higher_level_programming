431f9818bb023216025981d944356cd7b56b34c5
Zakaria Bakour <husa.bakour02@gmail.com>
2023-12-06 17:08:34 -0800

    wip

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,26 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    dic_keys = a_dictionary.keys()
-    for ikey in dic_keys:
-        val = a_dictionary[ikey]
-        if not val:
-            a_dictionary[ikey] = value
-        else:
-             a_dictionary[ikey] = value
-
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
-
-print("--")
-print("--")
-
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
-
+    a_dictionary[key] = value
+    return a_dictionary
