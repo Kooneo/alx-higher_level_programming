1c40bf5be86da521bba33f2b82b115c3b479502b
Zakaria Bakour <husa.bakour02@gmail.com>
2023-12-06 16:52:27 -0800

    wip

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return dict(a_dictionary)
+    sorted_dic = dict(a_dictionary)
+    for key, value in range(len(sorted_dic)):
+        print("{}: {}".format(key, value))
